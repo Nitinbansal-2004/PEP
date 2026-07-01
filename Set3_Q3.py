949232158e55a0570583e22d60e07bfd9f257f37
Nitinbansal-2004 <nb0350422@gmail.com>
2026-07-01 10:18:55 +0530

    Updated PEP solutions

--- Set3_Q3.py
+++ Set3_Q3.py
@@ -3,11 +3,14 @@ pnl = []
 for i in range(n):
     pnl.append(int(input()))
 k = int(input())
-n = len(pnl)
-
 max_sum = 0
 for i in range(n):
+    curr_sum=0
     for j in range(i, min(i + k, n)):
-        max_sum += pnl[j]
+        curr_sum += pnl[j]
+    if max_sum<curr_sum:
+        max_sum=curr_sum
         
 print(max_sum)
+
+
